bccd720f88da5a54878410a56c5c39c3c1fb9d0f
Rahul Kumar <rkrahul1775@gmail.com>
2025-03-24 13:04:45 +0530

    Topic Service added

--- src/main/java/com/rahul/topic/TopicController.java
+++ src/main/java/com/rahul/topic/TopicController.java
@@ -1,21 +1,20 @@
 package com.rahul.topic;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 	
+	@Autowired
+	private TopicService topicService;
+	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-				new Topic("spring","Spring FrameWork","Spring FrameWork Description"),
-				new Topic("java","Core Java","Core Java Description"),
-				new Topic("javascript","Javascript","Javascript Description")
-				);
+		return topicService.getAllTopics();
 	}
 
 }
